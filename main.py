19ce2b0d10f1b95246b45aef1511749193b08619
John Doe <johndoe@example.com>
2024-12-05 09:05:30 +0900

    修正

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
 import sys
+import re
 from kanjiconv import KanjiConv
+import jaconv
 
 kanji_conv = KanjiConv(separator="")
 
 
+def split_text_by_pattern(text):
+    # pattern = re.compile(r"[a-zA-Z0-9]+")
+    pattern = re.compile(r"[a-zA-Z0-9;；「」:　 ]+")
+    results = []
+    last_end = 0
+
+    for match in pattern.finditer(text):
+        start, end = match.span()
+
+        # 英数字以外の部分（マッチの前）を追加
+        if start > last_end:
+            non_alpha = text[last_end:start]
+            results.append(("non_alphanumeric", non_alpha))
+
+        # 英数字部分を追加
+        results.append(("alphanumeric", match.group()))
+        last_end = end
+
+    # 最後の英数字以外の部分を追加
+    if last_end < len(text):
+        results.append(("non_alphanumeric", text[last_end:]))
+
+    return results
+
+
+def convert_time_format(text):
+    # 半角数字+「とき」のパターンを「じ」に置換
+    pattern = r"(\d+)とき"
+    result = re.sub(pattern, r"\1じ", text)
+    return result
+
+
 def convert_hiragana(text):
-    return kanji_conv.to_hiragana(text)
+    text = text.replace("私は", "わたしは")
+    text = jaconv.z2h(text, kana=False, digit=True, ascii=True)
+    results = split_text_by_pattern(text)
+    result = ""
+    for v in results:
+        if v[0] == "non_alphanumeric":
+            result += kanji_conv.to_hiragana(v[1])
+        else:
+            result += v[1]
+
+    return result
 
 
 def make_questions_text():
@@ -25,6 +69,7 @@ def make_questions_text():
     new_lst = []
     for eng, jpn in pairs:
         result = convert_hiragana(jpn)
+        result = convert_time_format(result)
         new_lst.append([eng, jpn, result])
 
     with open("./public/questions.txt", "w", encoding="utf-8") as f:
