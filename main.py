bb63782e0d0f5ea1b4135cb4cdf36f716ae49b4b
John Doe <johndoe@example.com>
2024-12-13 15:56:35 +0900

    更新

--- main.py
+++ main.py
@@ -79,21 +79,14 @@ def convert_hiragana(text):
 def make_questions_text():
     """ひらがなにしながらquestions.txt作成"""
 
-    pairs = []
-    with open("origin.txt", "r", encoding="utf-8") as file:
-        current_pair = []
-        for line in file:
-            line = line.strip()
-            if line and line != "----------":
-                current_pair.append(line)
-                if len(current_pair) == 2:
-                    pairs.append(tuple(current_pair))
-                    current_pair = []
-
     new_lst = []
-    for eng, jpn in pairs:
-        result = convert_hiragana(jpn)
-        new_lst.append([eng, jpn, result])
+
+    with open("origin.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            eng, jpn = line.split("★")
+            result = convert_hiragana(jpn)
+            new_lst.append([eng, jpn, result])
 
     with open("./public/questions.txt", "w", encoding="utf-8") as f:
         for v in new_lst:
@@ -101,27 +94,4 @@ def make_questions_text():
             f.write(data)
 
 
-def check_origin_fomat():
-    """origin.txtの形式は正しいかチェック"""
-
-    with open("origin.txt", "r", encoding="utf-8") as f:
-        total = 1
-        i = 1
-        for line in f:
-            line = line.strip()
-            if i % 3 == 0:
-                if line != "----------":
-                    print(total, line)
-                    sys.exit()
-                i = 1
-            else:
-                i += 1
-
-            total += 1
-
-    print("終了")
-
-
 make_questions_text()
-
-# check_origin_fomat()
